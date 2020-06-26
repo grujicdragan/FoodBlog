37d57908cc70c78013df284ce2642b73cb327480
grujicdragan <dragan_grujic93@hotmail.com>
2020-06-26 11:39:27 +0200

    appjs

---
+++ app.js
@@ -0,0 +1,16 @@
+const path = require("path");
+const express = require("express");
+
+
+const port = process.env.PORT | 3000;
+
+
+const app = express();
+
+const publicDirectoryPath = path.join(__dirname, "../public");
+
+app.use(express.static(publicDirectoryPath));
+
+app.listen(port, () => {
+    console.log(`Server is up and running on port ${port}`);
+});

8014c85d9ffa7d95735d07ef06e28285c485274e
grujicdragan <dragan_grujic93@hotmail.com>
2020-06-26 13:20:21 +0200

    path

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const path = require("path");
 const express = require("express");
 
 
-const port = process.env.PORT | 3000;
+const port = process.env.PORT || 3000;
 
 
 const app = express();
 
-const publicDirectoryPath = path.join(__dirname, "../public");
+const publicDirectoryPath = path.join(__dirname, "public");
 
 app.use(express.static(publicDirectoryPath));
 
